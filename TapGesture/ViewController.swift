72555670a0775ee3bbbf9a278d8fd841e49d9ec7
Steven Gunawan <stevengunawan1512@gmail.com>
2019-07-11 14:16:52 +0700

    Initial Commit

---
+++ TapGesture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TapGesture
+//
+//  Created by Steven Gunawan on 11/07/19.
+//  Copyright © 2019 Steven Gunawan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
