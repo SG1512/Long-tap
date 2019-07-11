cacc6fa38faec6d7d994d283a04dd4c5bcd37053
Steven Gunawan <stevengunawan1512@gmail.com>
2019-07-11 15:16:49 +0700

    long tap

--- TapGesture/ViewController.swift
+++ TapGesture/ViewController.swift
@@ -10,11 +10,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+
+    @IBOutlet weak var rectangleView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func didlongtap(_ sender: UILongPressGestureRecognizer) {
+        rectangleView.backgroundColor = #colorLiteral(red: 0.521568656, green: 0.1098039225, blue: 0.05098039284, alpha: 1)
+    }
+    
 }
 
